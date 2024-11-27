4d2a669a26240bf0409feb0d8ad5eb890cef31de
Spencer Miller <spencerrmiller3@gmail.com>
2024-11-26 19:38:00 -0700

    connected to db

--- server.js
+++ server.js
@@ -17,4 +17,26 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirn
+    res.sendFile(path.join(__dirname, 'public', 'login.html'))
+})
+
+app.post('/api/v1/login', (req, res) => {
+
+})
+
+
+
+
+//connect to server and databse
+mongoose.connect(`mongodb+srv://${username}:${password}@schoolsystem.bxba5.mongodb.net/?retryWrites=true&w=majority&appName=SchoolSystem`)
+    .then(() => {
+        console.log('connected to database')
+    })
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server listening on localhost:${port}`)
+        })
+    })
+    .catch(() => {
+        console.log('Connection failed')
+    })
